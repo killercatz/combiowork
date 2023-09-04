d3151bce79573c55d2e624d5161628bfc5caf1d2
killercatz <89695701+killercatz@users.noreply.github.com>
2023-09-04 09:53:18 +0700

    Add AOV and correlation graph

--- Work3_3.R
+++ Work3_3.R
@@ -11,21 +11,46 @@ library(palmerpenguins)
 #have the same or different bill length, bill depth, flipper length, and body mass?
 
 selected_columns <- c("bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g")
-  
-select_penguin <- data.frame(na.omit(penguins %>% filter(species == 'Adelie') 
-                          %>% select(bill_length_mm)))
-anova_result <- aov(select_penguin ~ species, data = penguins)
 
-anova_summary <- tidy(anova_result)
-print(anova_summary)
+for (col in selected_columns) {
+  
+  anova_result <- aov(as.formula(paste(col, " ~ species")), data = penguins)
+
+  print(tidy(anova_result))
+  
+}
 
 #Q4: Please find pairwise relationship between bill length, bill depth,
 #flipper length, and body mass. Are these relationship the same or different between
 #the three penguin species?
-  
-par(mfrow = c(2, 2))
 
-  boxplot(bill_length_mm ~ species, data = penguins)
-  boxplot(bill_depth_mm ~ species, data = penguins)
-  boxplot(flipper_length_mm ~ species, data = penguins)
-  boxplot(body_mass_g ~ species, data = penguins)
+# Text Version
+for (i in 1:3) {
+  
+  target_species <- switch(i, "Adelie", "Gentoo", "Chinstrap")
+  
+  select_penguin <- as.matrix(na.omit(penguins  %>% filter(species == target_species) 
+                                      %>% select(bill_length_mm,
+                                                            bill_depth_mm,
+                                                            flipper_length_mm,
+                                                            body_mass_g)))
+
+  corr <- cor(select_penguin)
+  cat('The correlation for', target_species, 'is\n')
+  print(corr)
+}
+
+# Graph Version
+pairs(as.matrix(na.omit(penguins %>% select(bill_length_mm,
+                                   bill_depth_mm,
+                                   flipper_length_mm,
+                                   body_mass_g))),
+      main = "Correlation of penguin species",
+      gap = 1,
+      font.lables = 1,
+      pch = 21,
+      bg = c("red3","blue","cyan4"),
+      col = c("red3","blue","cyan4"),
+      lower.panel = panel.smooth,
+      upper.panel = NULL)
+
