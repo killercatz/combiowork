2248443b8737580060920d215625619e2653277a
killercatz <89695701+killercatz@users.noreply.github.com>
2023-10-06 15:51:57 +0700

    Added progress bar

--- ExamPart1.R
+++ ExamPart1.R
@@ -4,12 +4,17 @@ data <- read.csv("examdata.csv")
 primer <- c('G7', 'G9', 'G21', 'G22', 'G23', 'G24', 'G25', 'G26', 'G27', 'G28', 'G29',
            'G30', 'G31', 'G32', 'G33', 'G35', 'G36', 'G40', 'G42')
 
-coll <-  data['1',] %>% select(Sample)
-
-wordremove <- paste(primer, collapse = "|")
-output <- gsub(wordremove, "", coll)
+#coll <-  data['1',] %>% select(Sample)
+#wordremove <- paste(primer, collapse = "|")
+#output <- gsub(wordremove, "", coll)
 
 totalrow <- nrow(data)
+
+progressbar <- txtProgressBar(min = 0,
+                     max = totalrow,
+                     style = 3,
+                     width = 50,
+                     char = "▄") 
 
 for(number in 1:totalrow) {
   if(number == 1) {
@@ -18,8 +23,10 @@ for(number in 1:totalrow) {
     sample_name <- NULL
   } 
   
-  coll <-  data[number,] %>% select(Sample)
+  setTxtProgressBar(progressbar, number)
   
+  coll <-  data[number,] %>% select(Sample)
+  wordremove <- paste(primer, collapse = "|")
   output <- gsub(wordremove, "", coll)
   
   if(output == defaultvalue) {} else {
@@ -32,7 +39,9 @@ for(number in 1:totalrow) {
   
   if(number == totalrow) {
     unique_name <- unique(sample_name)
-    print("Completed.")
+    close(progressbar)
+    cat(" Completed\n")
+    print("The removed primer name are:")
+    print(unique_name)
   }
 }
-
